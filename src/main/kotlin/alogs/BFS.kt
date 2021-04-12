aa12d53ed7789b2e0b7c7d989219caf9c76fed35
ch8n <chetan.garg36@gmail.com>
2021-04-13 00:43:42 +0530

    :memo: added breadthTraversal graph

---
+++ src/main/kotlin/alogs/BFS.kt
@@ -0,0 +1,57 @@
+package alogs
+
+abstract class AbstractGraph<T> {
+    data class Edges<T>(val pointsTo: Vertex<T>)
+    data class Vertex<T>(val data: T, val edges: List<Edges<T>>)
+}
+
+interface Graph<T> {
+    fun breadthTraversal()
+    fun depthTraversal()
+}
+
+interface MutableGraph<T> : Graph<T>
+
+class GraphCollection
+
+fun main() {
+
+    /***
+     *
+     *  // TODO how to solve concurrent read write
+     *
+     *  val graph = graphOf(1,4,5,6,7,8,9)
+     *
+     *  val visits = mutableListOf<Edge>()
+     *  var explored = visit.toList() <<<<<< this is code noise | cause of concurrent read and write
+     *  val directEdges = list<Edge>()
+     *  var visitIndex = 0
+     *
+     *  // repeat all steps until all nodes are visited
+     *  while(explored.size == graph.size){
+     *
+     *  // visit a node -> put in visits
+     *  val subGraph: Vertex = graph.get(visitIndex)
+     *
+     *  //  find all the direct edges of visits[index] --> directEdges.addAll(edges)
+     *  val directEdges = subGraph.edges
+     *
+     *  // if --> visit contains directEdges[index] --> don't add to visit
+     *  // if --> visit not contains directEdges[index] --> add to visit
+     *  val notContainedEdges: List<Edges> = directEdges.filter { !visits.contains(it) }
+     *
+     *  // add not contained edges to visits
+     *  visits.addAll(notContainedEdges)
+     *  explored = visit.toList()
+     *
+     *  //print result of visits
+     *  println(explored[visitIndex])
+     *  ++visitIndex
+     *
+     *  }
+     *
+     *
+     *
+     */
+
+}
